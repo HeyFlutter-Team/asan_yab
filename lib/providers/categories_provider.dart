e86ac90a45b1b31c438cbd5e17aa5ece8ad655c9
Daniel97139 <mahdi.1001.500@gmail.com>
2023-07-29 05:12:20 -0700

    fixed something on detailsPage

--- lib/providers/categories_provider.dart
+++ lib/providers/categories_provider.dart
@@ -1,22 +1,36 @@
 import 'package:asan_yab/database/firebase_helper/category.dart';
 import 'package:asan_yab/repositories/categories_rep.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../database/firebase_helper/place.dart';
 
 class CategoriesProvider with ChangeNotifier {
   final categoriesRepository = CategoriesRepository();
 
   List<Category> _categories = [];
   List<Category> get categories => _categories;
-  set categories(List<Category> categories){
+  set categories(List<Category> categories) {
     _categories = categories;
     notifyListeners();
   }
-  Future<List<Category>> getCategories () async {
+
+  Future<List<Category>> getCategories() async {
     final newCategories = await categoriesRepository.fetchCategories();
     _categories = newCategories;
     notifyListeners();
     return _categories;
-    
   }
- 
+
+  Future<Place?> fetchSinglePlace(String id) async {
+    final database = FirebaseFirestore.instance;
+    try {
+      final querySnapshot = await database.collection('Places').doc(id).get();
+      final place = Place.fromJson(querySnapshot.data()!);
+      return place;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
 }
