70ce577cecda1b07d5f9a72718d58c91e43c5094
HarisAtiq2762 <harisatiq14@gmail.com>
2023-08-23 04:24:45 +0500

    Building CICD Pipeline for android

--- android/app/src/main/kotlin/com/heyflutter/asanYab/MainActivity.kt
+++ android/app/src/main/kotlin/com/heyflutter/asanYab/MainActivity.kt
@@ -1,15 +1,6 @@
-<<<<<<< HEAD:android/app/src/main/kotlin/com/example/asan_yab/MainActivity.kt
 package com.heyflutter.asanYab
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
-=======
-package com.heyflutter.asanYab
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
->>>>>>> 87ef5619ef04ca3d45448e60db9f0b77a03f3b29:android/app/src/main/kotlin/com/heyflutter/asanYab/MainActivity.kt
