1f823da511257e5acd7b9921531610a208fcb7db
HarisAtiq2762 <harisatiq15@gmail.com>
2023-10-03 22:41:10 +0500

    adding plugins into the repository

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,36 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:location_web/location_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
