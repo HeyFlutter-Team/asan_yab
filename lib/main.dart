744e68a4e529e1e219c3fb66ed68818e979ec09a
Hojjat132 <alihijjat@gmail.com>
2023-12-24 16:47:48 +0430

    'darksmode'

--- lib/main.dart
+++ lib/main.dart
@@ -136,7 +136,7 @@ class _MyAppState extends ConsumerState<MyApp> {
               fontFamily: 'Shabnam',
               appBarTheme: AppBarTheme(
                 backgroundColor: Theme.of(context).brightness == Brightness.dark
-                    ? Colors.black45
+                    ? Colors.black.withOpacity(0.1)
                     : Colors.white,
               )
               // Add other theme configurations here if needed
