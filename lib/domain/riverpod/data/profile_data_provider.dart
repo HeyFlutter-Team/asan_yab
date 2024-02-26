343a4b32a70127c6fc9bbf862b53c8196c50312e
Dell <a>
2024-02-26 12:46:17 +0430

    can add note from firebase to the new place page

--- lib/domain/riverpod/data/profile_data_provider.dart
+++ lib/domain/riverpod/data/profile_data_provider.dart
@@ -1,22 +1,22 @@
-// ignore_for_file: avoid_print
-
 import 'dart:io';
 
 import 'package:clipboard/clipboard.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../data/models/users.dart';
+
+final userDetailsProvider = StateNotifierProvider<ReadUserDetails, Users?>(
+  (ref) => ReadUserDetails(),
+);
 
 class ReadUserDetails extends StateNotifier<Users?> {
   ReadUserDetails({Users? user}) : super(user);
 
-  Future<void> getCurrentUserData(BuildContext context) async {
+  Future<void> getCurrentUserData() async {
     try {
       final user = FirebaseAuth.instance.currentUser;
 
@@ -26,10 +26,16 @@ class ReadUserDetails extends StateNotifier<Users?> {
             .doc(user.uid)
             .get();
 
-        state = Users.fromMap(userSnapshot.data()!);
+        if (userSnapshot.exists) {
+          state = Users.fromMap(userSnapshot.data()!);
+        } else {
+          print('User data not found for user ID: ${user.uid}');
+        }
+      } else {
+        print('Current user is null');
       }
-    } catch (e) {
-      print('Error getting current user data: $e');
+    } catch (e, stackTrace) {
+      print('Error getting current user data: $e\n$stackTrace');
     }
   }
 
@@ -41,15 +47,6 @@ class ReadUserDetails extends StateNotifier<Users?> {
     FlutterClipboard.copy(text);
   }
 }
-
-
-final userDetailsProvider =
-    StateNotifierProvider<ReadUserDetails, Users?>((ref) => ReadUserDetails());
-
-
-
-
-
 
 class ImageState {
   File? image;
@@ -76,21 +73,19 @@ class ImageState {
 }
 
 class ImageNotifier extends StateNotifier<ImageState> {
-  Ref? refs;
   ImageNotifier() : super(ImageState());
-
   final ImagePicker _imagePicker = ImagePicker();
 
   Future<void> pickImage(ImageSource source) async {
     try {
-     final pickedImage = await _imagePicker.pickImage(source: source);
+      final pickedImage = await _imagePicker.pickImage(source: source);
       if (pickedImage == null) return;
 
-       final imageTemp = File(pickedImage.path);
-      state = state.copyWith(image: imageTemp,imageUrl: pickedImage.path);
+      final imageTemp = File(pickedImage.path);
+      state = state.copyWith(image: imageTemp, imageUrl: pickedImage.path);
       uploadFile();
-    } catch (e) {
-      print('Failed to pick image: $e');
+    } catch (e, stackTrace) {
+      print('Failed to pick image: $e\n$stackTrace');
     }
   }
 
@@ -103,22 +98,23 @@ class ImageNotifier extends StateNotifier<ImageState> {
     final ref = FirebaseStorage.instance.ref().child(path);
 
     try {
-      state = state.copyWith(uploadTask: ref.putFile(state.image!));
+      final uploadTask = ref.putFile(state.image!);
+      state = state.copyWith(uploadTask: uploadTask);
 
-      final snapshot = await state.uploadTask!.whenComplete(() {});
+      final snapshot = await uploadTask.whenComplete(() {});
       final uploadedImageUrl = await snapshot.ref.getDownloadURL();
 
-      FirebaseFirestore.instance.collection('User').doc(user.uid).update({
+      await FirebaseFirestore.instance.collection('User').doc(user.uid).update({
         'imageUrl': uploadedImageUrl,
       });
 
       state = state.copyWith(imageUrl: uploadedImageUrl);
-    } catch (e) {
-      print('Error uploading image: $e');
+    } catch (e, stackTrace) {
+      print('Error uploading image: $e\n$stackTrace');
     }
   }
 }
 
-final imageProvider = StateNotifierProvider<ImageNotifier, ImageState>((ref) {
-  return ImageNotifier();
-});
+final imageProvider = StateNotifierProvider<ImageNotifier, ImageState>(
+  (ref) => ImageNotifier(),
+);
